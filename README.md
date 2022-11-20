b899bc0c49f5fb8e4d1eb1a6db8206e9be85b82b
elanticrypt0 <elanticrypt0@gmail.com>
2022-11-20 18:47:58 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+<p align="center">
+  <a href="http://nestjs.com/" target="blank"><img src="https://nestjs.com/img/logo-small.svg" width="200" alt="Nest Logo" /></a>
+</p>
+
+# Ejecutar en desarrollo
+
+1. Clonar el repo
+2. Ejecutar
+
+```
+yarn install
+```
+
+3. Instalar CLI instalado
+
+```
+npm i -g @nestjs/cli
+```
+
+4. Levantar la DB. En este caso es mongo
+
+```
+docker-compose -up -d
+```
+
+# Stack usado
+
+* Nest
+* Mongo
---
+++ src/pokemon/pokemon.service.ts
@@ -0,0 +1,87 @@
+import { BadRequestException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { isValidObjectId, Model } from 'mongoose';
+import { CreatePokemonDto } from './dto/create-pokemon.dto';
+import { UpdatePokemonDto } from './dto/update-pokemon.dto';
+import { Pokemon } from './entities/pokemon.entity';
+
+@Injectable()
+export class PokemonService {
+
+  constructor(
+    @InjectModel( Pokemon.name )
+    private readonly pokemonModel:Model<Pokemon>
+  ){}
+
+  async create(createPokemonDto: CreatePokemonDto) {
+    createPokemonDto.name=createPokemonDto.name.toLocaleLowerCase();
+
+    try {
+      const pokemon = await this.pokemonModel.create(createPokemonDto);      
+      return createPokemonDto;
+    } catch (error) {
+      this.duplicatePokemonException(error);
+    }
+
+  }
+
+  findAll() {
+    const pokemons = this.pokemonModel.find();
+    return pokemons;
+  }
+
+  async findOne(term: string) {
+
+
+    let pokemon:Pokemon;
+
+    if( !isNaN( +term ) ){
+      pokemon= await this.pokemonModel.findOne({no:term});
+    }
+
+    if( isValidObjectId(term) ){
+      pokemon= await this.pokemonModel.findById(term);
+    }
+
+    if(!pokemon){
+      pokemon= await this.pokemonModel.findOne({name:term.toLocaleLowerCase().trim()});
+    }
+
+    if(!pokemon) throw new NotFoundException(`The pokemon '${term}' dosen't exist`);
+
+    return pokemon;
+  
+  }
+
+  async update(term: string, updatePokemonDto: UpdatePokemonDto) {
+    const pokemon = await this.findOne(term);
+    if( updatePokemonDto.name ){
+      updatePokemonDto.name=updatePokemonDto.name.toLocaleLowerCase().trim();
+    }
+    
+    try {
+      await pokemon.updateOne(updatePokemonDto, {new:true});
+    } catch (error) {
+      this.duplicatePokemonException(error);
+    }
+    
+    return {...pokemon.toJSON(),...updatePokemonDto};
+  }
+
+  async remove(id:string) {
+    // const result= await this.pokemonModel.findByIdAndDelete(id);
+    const { deletedCount } = await this.pokemonModel.deleteOne({_id:id});
+    if(deletedCount === 0){
+      throw new BadRequestException(`There is no pokemon with id '${id}'`);
+    }
+    return;
+  }
+
+  private duplicatePokemonException(error:any){
+    if (error.code === 11000){
+      throw new BadRequestException(`Pokemon already exist in DB. ${ JSON.stringify( error.keyValue ) }`);
+    }
+    console.log(error);
+    throw new InternalServerErrorException(`Can't create the register in DB`);
+  }
+}
