394b747adcfd4a97a29c3aa58703889ef6387567
elanticrypt0 <elanticrypt0@gmail.com>
2022-11-20 21:11:35 -0300

    AxiosAdapter creado

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,20 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { PokeResponse } from './interfaces/poke-response.interface';
-import axios, { AxiosInstance } from 'axios';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { AxiosAdapter } from '../common/adapters/axios.adapter';
 
 
 @Injectable()
 export class SeedService {
 
-  // private readonly http=new HttpAdapter();
-  private readonly axios:AxiosInstance = axios;
-
   constructor(
     @InjectModel( Pokemon.name )
-    private readonly pokemonModel:Model<Pokemon>
+    private readonly pokemonModel:Model<Pokemon>,
+    private readonly http:AxiosAdapter
   ){}
 
   async executeSeed(){
@@ -22,7 +20,7 @@ export class SeedService {
     this.pokemonModel.deleteMany(); // Delete * From pokemons
 
     // const { data } = await this.http.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=1');
-    const {data} = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=60');
+    const data = await this.http.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=200');
 
     let pokemonsToInsert:{name:string,no:number}[]=[];
 
